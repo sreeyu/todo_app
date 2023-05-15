59b34d4aeeeabdf226dac4835e57760e472199e4
sreey <sreelakshmi12898@gmail.com>
2023-05-15 17:19:58 +0530

    Add feature to avoid blank todos

--- src/components/TodoInput/TaskInput.jsx
+++ src/components/TodoInput/TaskInput.jsx
@@ -5,20 +5,28 @@ import TaskButton from './TaskButton';
 function TaskInput(props){
 
     const [enteredTodo, setEnteredTodo] = useState('');
+    const [isValid, setIsvalid] = useState(true);
 
     const getTodoInput = (event) => {
+        if(event.target.value.trim().length !== 0){
+            setIsvalid(true);
+        }
         setEnteredTodo(event.target.value);
     };
 
     const submitForm = (event) =>{
         event.preventDefault();
+        if(enteredTodo.trim().length === 0){
+            setIsvalid(false);
+            return;
+        }
         props.onInput(enteredTodo);
     };
     
     return (
         <form onSubmit={submitForm}>
-            <div className="form-input">
-                <label >Enter Task</label>
+            <div className={`form-input ${isValid ? '' : 'invalid'}`}>
+                <label>Enter Task</label>
                 <input type="text" onChange={getTodoInput} />
             </div>
             <TaskButton type='submit' >Add Task</TaskButton>
